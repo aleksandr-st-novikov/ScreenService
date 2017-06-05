f057126ec30b6bd7b218676398b2ac0125b0f25a
aleksandr-st-novikov <aleksandr.st.novikov@gmail.com>
2017-06-05 11:46:14 +0300

    0

---
+++ install/install.cmd
@@ -0,0 +1,3 @@
+"c:\Program Files (x86)\SceenService\installutil.exe" "c:\Program Files (x86)\SceenService\ScreenService.exe"
+rem "c:\Program Files (x86)\SceenService\installutil.exe" /u "c:\Program Files (x86)\SceenService\ScreenService.exe"
+pause
