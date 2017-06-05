a87ea2022481685ebcf5aa17937e7e4510347687
aleksandr-st-novikov <aleksandr.st.novikov@gmail.com>
2017-06-05 10:20:47 +0300

    Add project files.

---
+++ ScreenService/ScreenCervice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ScreenService
+{
+    public partial class ScreenCervice : ServiceBase
+    {
+        public ScreenCervice()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
