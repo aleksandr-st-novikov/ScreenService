a35b6e2691e69ef7b755de26528936226e788da9
aleksandr-st-novikov <aleksandr.st.novikov@gmail.com>
2017-06-05 11:45:20 +0300

    0

---
+++ Domain/Methods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Domain
+{
+    public class Methods
+    {
+        public static void GetScreen()
+        {
+            using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+                {
+                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                     Screen.PrimaryScreen.Bounds.Y,
+                                     0, 0,
+                                     bmpScreenCapture.Size,
+                                     CopyPixelOperation.SourceCopy);
+                }
+                ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Jpeg);
+
+                EncoderParameters parameters = new EncoderParameters(1);
+                parameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 40L);
+                bmpScreenCapture.Save("e:\\ScreenService\\" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".jpg", codecInfo, parameters);
+            }
+        }
+
+        //public static void GetScreen()
+        //{
+        //    Desktop userDesk = new Desktop();
+        //    userDesk.BeginInteraction();
+        //    string path = @"D:\Screen\";
+        //    if (!Directory.Exists(path))
+        //        Directory.CreateDirectory(path);
+
+        //    string fileName = string.Format("SCR-{0:yyyy-MM-dd_hh-mm-ss-tt}.png", DateTime.Now);
+        //    string filePath = path + fileName;
+        //    bmpScreenshot = CaptureScreen.GetDesktopImage();
+        //    bmpScreenshot.Save(filePath, ImageFormat.Png);
+        //    userDesk.EndInteraction();
+        //}
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
--- ScreenService/ScreenCervice.cs
+++ ScreenService/ScreenCervice.cs
@@ -1,27 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.ServiceProcess;
-using System.Text;
+using System.Timers;
+using System.Windows.Forms;
 
 namespace ScreenService
 {
     public partial class ScreenCervice : ServiceBase
     {
+        private System.Timers.Timer timer = null;
+
         public ScreenCervice()
         {
             InitializeComponent();
+
+            timer = new System.Timers.Timer(30000);//создаём объект таймера 
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
         }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                Domain.Methods.GetScreen();
+            }
+            catch(Exception ex)
+            { }
+        }
+
+        
 
         protected override void OnStart(string[] args)
         {
+            timer.Start();
         }
 
         protected override void OnStop()
         {
+            timer.Stop();
         }
+
+        
     }
 }
